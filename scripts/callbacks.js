a0163b448129eb9bd09100227deab4993da7cdce
Jeff Hansen <dscheffy@gmail.com>
2018-01-19 15:23:08 +0530

    Add search session id and option not chosen to query string

--- scripts/callbacks.js
+++ scripts/callbacks.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $.getJSON("hunger.json", function(choice) {
+  $.getJSON("hunger.json" + location.search, function(choice) {
     $.choice=choice;
     $("#img-a").attr("src","images/" + choice.a); 
     $("#img-b").attr("src","images/" + choice.b); 
-    $("#want-a").attr("href","success.html?chosen=" + choice.a); 
-    $("#want-b").attr("href","success.html?chosen=" + choice.b); 
-    $("#prefer-a").attr("href","index.html?chosen=" + choice.a); 
-    $("#prefer-b").attr("href","index.html?chosen=" + choice.b); 
+    $("#want-a").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
+    $("#prefer-a").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
+    $("#want-b").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
+    $("#prefer-b").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
   });
 });
