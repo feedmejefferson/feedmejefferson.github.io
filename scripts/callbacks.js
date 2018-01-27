ad7d9281b4615699fc1489b5f1ebf11b17b1903f
Jeff Hansen <dscheffy@gmail.com>
2018-01-27 17:36:15 +0530

    Attempt at geting CORS to work

--- scripts/callbacks.js
+++ scripts/callbacks.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
-  $.getJSON("hunger.json" + location.search, function(choice) {
-    $.choice=choice;
-    $("#img-a").attr("src","images/" + choice.a); 
-    $("#img-b").attr("src","images/" + choice.b); 
-    $("#want-a").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
-    $("#prefer-a").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
-    $("#want-b").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
-    $("#prefer-b").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
+  $.ajax({
+//    type: 'GET',
+    dataType: "jsonp",
+//    contentType: 'text/plain',
+//    xhrFields: {
+//      withCredentials: false
+//    },
+    url: "http://feedmejefferson.com:9000/hunger.json" + location.search,
+//    crossDomain: true,
+    success: function(choice) {
+      $.choice=choice;
+      $("#img-a").attr("src","images/" + choice.a); 
+      $("#img-b").attr("src","images/" + choice.b); 
+      $("#want-a").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
+      $("#prefer-a").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
+      $("#want-b").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
+      $("#prefer-b").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
+    }
   });
 });
