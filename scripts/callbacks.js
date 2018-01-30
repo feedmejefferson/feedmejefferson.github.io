cbbfdeedab460484e3e746e3580540825f77fe7d
Jeff Hansen <dscheffy@gmail.com>
2018-01-30 14:39:11 +0530

    Add local hour of day to query params

--- scripts/callbacks.js
+++ scripts/callbacks.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
     url: "http://hunger.feedmejefferson.com:9000/hunger.json" + location.search,
     crossDomain: true,
     success: function(choice) {
-      $.choice=choice;
+      var hour = new Date().getHours();
       $("#img-a").attr("src","images/" + choice.a); 
       $("#img-b").attr("src","images/" + choice.b); 
-      $("#want-a").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
-      $("#prefer-a").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b); 
-      $("#want-b").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
-      $("#prefer-b").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a); 
+      $("#want-a").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b + "&hour=" + hour); 
+      $("#prefer-a").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.a + "&notChosen=" + choice.b + "&hour=" + hour); 
+      $("#want-b").attr("href","success.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a + "&hour=" + hour); 
+      $("#prefer-b").attr("href","index.html?searchSession=" + choice.searchSession + "&chosen=" + choice.b + "&notChosen=" + choice.a + "&hour=" + hour); 
     }
   });
 });
